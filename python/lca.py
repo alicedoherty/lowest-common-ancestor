32fa4ced71c160874b315d51bf9227a989e84862
Alice Doherty <66687032+alicedoherty@users.noreply.github.com>
2021-10-28 20:32:56 +0100

    added DAG data structure and basic test

--- python/lca.py
+++ python/lca.py
@@ -1,13 +1,14 @@
-# Python program to find LCA of two nodes of a binary tree.
-# Code is adapted from GeeksForGeeks.org
+# Python program to find LCA of two nodes of a directed acyclic graph.
 # Assumes that each node has a unique key.
 
 class Node:
 
+    # Key is the value of the node.
+
     def __init__(self, key):
         self.key = key
-        self.left = None
-        self.right = None
+        self.parents = []
+        self.children = []
 
 
 def findLCA(root, n1, n2):
@@ -21,10 +22,10 @@ def findLCA(root, n1, n2):
         n2 - the second Node
     """
 
-    if isNodePresent(root, n1) and isNodePresent(root, n2):
-        return recursiveFindLCA(root, n1, n2)
-    else:
-        return None
+    # if isNodePresent(root, n1) and isNodePresent(root, n2):
+    #     return recursiveFindLCA(root, n1, n2)
+    # else:
+    #     return None
 
 
 def recursiveFindLCA(root, n1, n2):
@@ -44,35 +45,54 @@ def recursiveFindLCA(root, n1, n2):
     if root.key == n1 or root.key == n2:
         return root
 
-    left_lca = recursiveFindLCA(root.left, n1, n2)
-    right_lca = recursiveFindLCA(root.right, n1, n2)
+    if n1 == n2:
+        return n1.key
 
-    if left_lca and right_lca:
-        return root
+    lca = [ ]
+    
+    for x in range(len(n1.parents)):
+        for y in range(len(n2.parents)):
+            if(n1.parents[x].key == n2.parents[y].key):
+                lca.append(n1.parents[x].key)
+    
+    if(lca == []):
+        if(n1.key > n2.key):
+            lca.append(recursiveFindLCA(root, n1.parents[0], n2))
+        else:
+            lca.append(recursiveFindLCA(root, n1, n2.parents[0]))
 
-    return left_lca if left_lca is not None else right_lca
+    return max(lca)
 
 
-def isNodePresent(root, key):
-    """
-    Checks if a key is present in a given binary tree.
+    # left_lca = recursiveFindLCA(root.left, n1, n2)
+    # right_lca = recursiveFindLCA(root.right, n1, n2)
 
-    Parameters:
-        root - the root Node of the binary tree
-        key - the value of the Node to check
-    """
+    # if left_lca and right_lca:
+    #     return root
 
-    if root is None:
-        return False
+    # return left_lca if left_lca is not None else right_lca
 
-    if root.key == key:
-        return True
 
-    left = isNodePresent(root.left, key)
+# def isNodePresent(root, key):
+#     """
+#     Checks if a key is present in a given binary tree.
 
-    if left:
-        return True
+#     Parameters:
+#         root - the root Node of the binary tree
+#         key - the value of the Node to check
+#     """
 
-    right = isNodePresent(root.right, key)
+#     if root is None:
+#         return False
 
-    return right
+#     if root.key == key:
+#         return True
+
+#     left = isNodePresent(root.left, key)
+
+#     if left:
+#         return True
+
+#     right = isNodePresent(root.right, key)
+
+#     return right
--- python/test_lca.py
+++ python/test_lca.py
@@ -4,94 +4,112 @@ import lca
 
 class TestLCA(unittest.TestCase):
 
-    def test_basic_tree(self):
-        # Test basic LCA queries with below binary tree.
-        #
-        #          1
-        #        /   \
-        #      2       3
-        #    /   \   /   \
-        #   4     5 6     7
+    # def test_basic_tree(self):
+    #     # Test basic LCA queries with below binary tree.
+    #     #
+    #     #          1
+    #     #        /   \
+    #     #      2       3
+    #     #    /   \   /   \
+    #     #   4     5 6     7
 
-        root = lca.Node(1)
-        root.left = lca.Node(2)
-        root.right = lca.Node(3)
-        root.left.left = lca.Node(4)
-        root.left.right = lca.Node(5)
-        root.right.left = lca.Node(6)
-        root.right.right = lca.Node(7)
+    #     root = lca.Node(1)
+    #     root.left = lca.Node(2)
+    #     root.right = lca.Node(3)
+    #     root.left.left = lca.Node(4)
+    #     root.left.right = lca.Node(5)
+    #     root.right.left = lca.Node(6)
+    #     root.right.right = lca.Node(7)
 
-        result1 = lca.findLCA(root, 4, 5).key
-        result2 = lca.findLCA(root, 5, 3).key
+    #     result1 = lca.findLCA(root, 4, 5).key
+    #     result2 = lca.findLCA(root, 5, 3).key
 
-        self.assertEqual(result1, 2)
-        self.assertEqual(result2, 1)
+    #     self.assertEqual(result1, 2)
+    #     self.assertEqual(result2, 1)
 
-    def test_straight_tree(self):
-        # Test funky looking binary tree.
-        #
-        #  1
-        #   \
-        #    2
-        #     \
-        #      3
-        #       \
-        #        4
+    # def test_straight_tree(self):
+    #     # Test funky looking binary tree.
+    #     #
+    #     #  1
+    #     #   \
+    #     #    2
+    #     #     \
+    #     #      3
+    #     #       \
+    #     #        4
 
-        root = lca.Node(1)
-        root.right = lca.Node(2)
-        root.right.right = lca.Node(3)
-        root.right.right.right = lca.Node(4)
+    #     root = lca.Node(1)
+    #     root.right = lca.Node(2)
+    #     root.right.right = lca.Node(3)
+    #     root.right.right.right = lca.Node(4)
 
-        result = lca.findLCA(root, 4, 3).key
-        self.assertEqual(result, 3)
+    #     result = lca.findLCA(root, 4, 3).key
+    #     self.assertEqual(result, 3)
 
-    def test_node_is_lca(self):
-        # Test when one of the provided nodes is the LCA.
-        # e.g LCA of 4 and 2 is 2
-        #
-        #          1
-        #        /   \
-        #      2       3
-        #    /   \
-        #   4     5
+    # def test_node_is_lca(self):
+    #     # Test when one of the provided nodes is the LCA.
+    #     # e.g LCA of 4 and 2 is 2
+    #     #
+    #     #          1
+    #     #        /   \
+    #     #      2       3
+    #     #    /   \
+    #     #   4     5
 
-        root = lca.Node(1)
-        root.left = lca.Node(2)
-        root.right = lca.Node(3)
-        root.left.left = lca.Node(4)
-        root.left.right = lca.Node(5)
+    #     root = lca.Node(1)
+    #     root.left = lca.Node(2)
+    #     root.right = lca.Node(3)
+    #     root.left.left = lca.Node(4)
+    #     root.left.right = lca.Node(5)
 
-        result = lca.findLCA(root, 4, 2).key
-        self.assertEqual(result, 2)
+    #     result = lca.findLCA(root, 4, 2).key
+    #     self.assertEqual(result, 2)
 
-    def test_node_not_included(self):
-        # Test when provided node is not in the binary tree.
-        #
-        #          1
-        #        /   \
-        #      2       3
-        #    /   \
-        #   4     5
+    # def test_node_not_included(self):
+    #     # Test when provided node is not in the binary tree.
+    #     #
+    #     #          1
+    #     #        /   \
+    #     #      2       3
+    #     #    /   \
+    #     #   4     5
 
-        root = lca.Node(1)
-        root.left = lca.Node(2)
-        root.right = lca.Node(3)
-        root.left.left = lca.Node(4)
-        root.left.right = lca.Node(5)
+    #     root = lca.Node(1)
+    #     root.left = lca.Node(2)
+    #     root.right = lca.Node(3)
+    #     root.left.left = lca.Node(4)
+    #     root.left.right = lca.Node(5)
 
-        result = lca.findLCA(root, 0, 2)
-        self.assertEqual(result, None)
+    #     result = lca.findLCA(root, 0, 2)
+    #     self.assertEqual(result, None)
 
-    def test_null_tree(self):
-        # Test when there is no binary tree (root is None).
+    # def test_null_tree(self):
+    #     # Test when there is no binary tree (root is None).
 
-        root = None
-        result = lca.findLCA(root, 1, 2)
-        self.assertEqual(result, None)
+    #     root = None
+    #     result = lca.findLCA(root, 1, 2)
+    #     self.assertEqual(result, None)
 
     def test_basic_dag(self):
-        # Can't write DAG test as there is no DAG data structure
+        root = lca.Node(1)
+        n2 = lca.Node(2)
+        n3 = lca.Node(3)
+        n4 = lca.Node(4)
+        n5 = lca.Node(5)
+        # n6 = lca.Node(6)
+        # n7 = lca.Node(7)
+
+        root.children = [n2, n3]
+        n2.parents = [root]
+        n2.children = [n4, n5]
+        n3.parents = [root, n5]
+        n4.parents = [n2]
+        n5.parents = [n2]
+        n5.children = [n3]
+
+        result = lca.recursiveFindLCA(root, n2, n5)
+        self.assertEqual(result, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
